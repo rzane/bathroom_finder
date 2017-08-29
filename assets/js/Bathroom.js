d2a41ba5844badee9595463681fe8d3862f0fbf5
Ray Zane <ray@promptworks.com>
2017-08-28 22:23:14 -0400

    Delete bathroom

--- assets/js/Bathroom.js
+++ assets/js/Bathroom.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { withProps } from 'recompose';
+import { graphql } from 'react-apollo';
+import { compose, withProps } from 'recompose';
 import Form from './Form';
 import asEditable from './asEditable';
+import {
+  BathroomsQuery,
+  DeleteBathroomMutation
+} from './queries';
 
 const Bathroom = ({
+  handleDelete,
   handleEdit,
   bathroom: {
     label,
@@ -26,7 +32,7 @@ const Bathroom = ({
     )}
 
     <footer className='card-footer'>
-      <a className='card-footer-item'>Delete</a>
+      <a className='card-footer-item' onClick={handleDelete}>Delete</a>
       <a className='card-footer-item' onClick={handleEdit}>Edit</a>
     </footer>
   </div>
@@ -36,4 +42,22 @@ const EditBathroom = withProps(() => ({
   title: 'Edit bathroom'
 }))(Form);
 
-export default asEditable(EditBathroom)(Bathroom);
+const enhance = compose(
+  asEditable(EditBathroom),
+  graphql(DeleteBathroomMutation, {
+    props ({ mutate, ownProps: { bathroom } }) {
+      return {
+        handleDelete (event) {
+          event.preventDefault();
+
+          return mutate({
+            variables: { id: bathroom.id },
+            refetchQueries: [{ query: BathroomsQuery }]
+          })
+        }
+      };
+    }
+  })
+);
+
+export default enhance(Bathroom);
