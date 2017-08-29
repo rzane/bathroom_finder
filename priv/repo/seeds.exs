7855ebf6663c766130fa0e26b619dd0435fb7a07
Ray Zane <ray@promptworks.com>
2017-08-29 11:16:07 -0400

    Add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,16 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     BathroomFinder.Repo.insert!(%BathroomFinder.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias BathroomFinder.Repo
+alias BathroomFinder.Bathroom
+
+Repo.insert! %Bathroom{
+  description: "The code is 1515.",
+  label: "Starbucks, Walnut Street, Philadelphia, PA, United States",
+  latitude: 39.949642,
+  longitude: -75.16741999999999
+}
+
+Repo.insert! %Bathroom{
+  description: "Vey clean.",
+  label: "PromptWorks, Chestnut Street, Philadelphia, PA, United States",
+  latitude: 39.9507393,
+  longitude: -75.1607353
+}
