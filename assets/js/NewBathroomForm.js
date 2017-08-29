add29d7693b9598c08d410224f830a86ba199696
Ray Zane <ray@promptworks.com>
2017-08-29 00:01:46 -0400

    Create a bathroom

--- assets/js/NewBathroomForm.js
+++ assets/js/NewBathroomForm.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { withProps } from 'recompose';
+import { graphql } from 'react-apollo';
 import BathroomForm from './BathroomForm';
+import {
+  BathroomsQuery,
+  CreateBathroomMutation
+} from './queries';
 
-const enhance = withProps(() => ({
-  title: 'Add a bathroom'
-}));
+const enhance = graphql(CreateBathroomMutation, {
+  props ({ mutate, ownProps: { setEditing } }) {
+    return {
+      title: 'Add a bathroom',
+
+      handleSave (attributes) {
+        return mutate({
+          variables: {
+            bathroom: attributes
+          },
+
+          update (store, { data: { createBathroom } }) {
+            const data = store.readQuery({
+              query: BathroomsQuery
+            });
+
+            store.writeQuery({
+              query: BathroomsQuery,
+              data: Object.assign({}, data, {
+                bathrooms: data.bathrooms.concat([createBathroom])
+              })
+            });
+          }
+        }).then(() => setEditing(false))
+          .catch(error => {
+            console.error(error);
+            alert('Failed to create bathroom!');
+          })
+      }
+    };
+  }
+});
 
 export default enhance(BathroomForm);
